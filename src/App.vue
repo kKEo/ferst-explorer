7c18ec4ec13e7a73a75b72241d54f9a9f04804da
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2024-06-14 01:15:15 +0200

    Use element plus component library

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,105 @@
 <template>
     <h1>💖 Hello Cruel World !!!</h1>
     <p>Welcome to your Electron application.</p>
+
+    <el-button type="primary" :icon="Edit">I am ElButton</el-button>
+    <el-button type="danger" :icon="Delete" @click="deleteItem"></el-button>
+
+    <el-tree
+    style="max-width: 600px"
+    :data="data"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+    />
+
   </template>
   
-  <script setup>
-  console.log('👋 This message is being logged by "App.vue", included via Vite');
-  </script>
+<script lang="ts" setup>
+
+import { Edit, Delete, Tools } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'
+
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+
+
+const deleteItem = () => {
+  ElMessage({
+    message: 'Item has been deleted',
+    grouping: true,
+    type: 'error'
+  })
+}
+
+interface Tree {
+  label: string
+  children?: Tree[]
+}
+
+const handleNodeClick = (data: Tree) => {
+  console.log(data)
+}
+
+const data: Tree[] = [
+  {
+    label: 'Level one 1',
+    children: [
+      {
+        label: 'Level two 1-1',
+        children: [
+          {
+            label: 'Level three 1-1-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 2',
+    children: [
+      {
+        label: 'Level two 2-1',
+        children: [
+          {
+            label: 'Level three 2-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 2-2',
+        children: [
+          {
+            label: 'Level three 2-2-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Level one 3',
+    children: [
+      {
+        label: 'Level two 3-1',
+        children: [
+          {
+            label: 'Level three 3-1-1',
+          },
+        ],
+      },
+      {
+        label: 'Level two 3-2',
+        children: [
+          {
+            label: 'Level three 3-2-1',
+          },
+        ],
+      },
+    ],
+  },
+]
+
+const defaultProps = {
+  children: 'children',
+  label: 'label',
+}
+
+</script>
